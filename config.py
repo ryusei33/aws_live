636fa30a13cefc934f31b29d58f1653db2aa938b
ryusei33 <166240161+ryusei33@users.noreply.github.com>
2024-04-16 13:00:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cfoyoiawe2dr.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lookwaiwy-employee"
+customregion = "us-east-1"
+
